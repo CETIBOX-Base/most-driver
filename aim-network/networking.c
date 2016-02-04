2b5ef376686cf9497980b334b796d54453c66ac6
Alex Riesen <alexander.riesen@cetitec.com>
2016-02-04 18:03:37 +0100

    Wake the netdev queue immediately if the INIC MAC is already known
    This breaks the carrier detection but at least things work on 81110

--- aim-network/networking.c
+++ aim-network/networking.c
@@ -216,6 +216,10 @@ static int most_nd_open(struct net_device *dev)
 	if (nd->is_mamac) {
 		nd->link_stat = 1;
 		netif_wake_queue(dev);
+	} else if (is_valid_ether_addr(dev->dev_addr)) {
+		netdev_info(dev, "INIC MAC %pM\n", dev->dev_addr);
+		nd->link_stat = 1;
+		netif_wake_queue(dev);
 	} else {
 		pr_info("%s: requesting INIC mac\n", __func__);
 		nd->iface->request_netinfo(nd->iface, nd->tx.ch_id);
