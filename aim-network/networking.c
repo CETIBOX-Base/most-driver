83655638d447226c7d38691595ccfd8e8328d7be
Alex Riesen <alexander.riesen@cetitec.com>
2016-02-10 18:51:40 +0100

    Add a local copy of ether_addr_copy
    The ancient kernel of the Renesas H2 platform does not have it yet.

--- aim-network/networking.c
+++ aim-network/networking.c
@@ -24,6 +24,28 @@
 #include "mostcore.h"
 #include "networking.h"
 
+/**
+ * ether_addr_copy - Copy an Ethernet address
+ * @dst: Pointer to a six-byte array Ethernet address destination
+ * @src: Pointer to a six-byte array Ethernet address source
+ *
+ * Please note: dst & src must both be aligned to u16.
+ */
+static inline void ether_addr_copy(u8 *dst, const u8 *src)
+{
+#if defined(CONFIG_HAVE_EFFICIENT_UNALIGNED_ACCESS)
+	*(u32 *)dst = *(const u32 *)src;
+	*(u16 *)(dst + 4) = *(const u16 *)(src + 4);
+#else
+	u16 *a = (u16 *)dst;
+	const u16 *b = (const u16 *)src;
+
+	a[0] = b[0];
+	a[1] = b[1];
+	a[2] = b[2];
+#endif
+}
+
 #define MEP_HDR_LEN 8
 #define MDP_HDR_LEN 16
 #define MAMAC_DATA_LEN (1024 - MDP_HDR_LEN)
